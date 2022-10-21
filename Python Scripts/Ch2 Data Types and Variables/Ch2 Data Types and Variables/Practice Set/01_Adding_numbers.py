7f3cb9f5565e8146c815647607272aa79cc884fb
Harsh Master <80234396+Harsh0707005@users.noreply.github.com>
2022-10-21 13:06:20 +0530

    Add files via upload

---
+++ Python Scripts/Ch2 Data Types and Variables/Ch2 Data Types and Variables/Practice Set/01_Adding_numbers.py
@@ -0,0 +1,7 @@
+# Write a Python program to add two numbers.
+
+a = input("Enter a number: ")
+a = int(a) # Converting 'a' to integer.
+b = input("Enter another number: ")
+b = int(b) # Converting 'b' to integer.
+print("The sum of the numbers is",a+b)
---
+++ Python Scripts/Ch2 Data Types and Variables/Ch2 Data Types and Variables/Practice Set/02_Remainder_of_numbers.py
@@ -0,0 +1,8 @@
+# Write a Python program to find remainder when a number is divided by 2.
+
+a = input("Enter first number: ")
+a = int(a) # Converting 'a' to integer.
+b = input("Enter second number: ")
+b = int(b) # Converting 'b' to integer.
+print("The remainder when first number is divided by the later is", a%b)
+# Here '%' symbol helps us to find the remainder when one number is divided by the other
