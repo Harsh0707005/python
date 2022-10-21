244c8eda5cca2a7630333310342cccf2700defd2
Harsh Master <80234396+Harsh0707005@users.noreply.github.com>
2022-10-21 13:06:50 +0530

    Delete Python Scripts/Ch2 Data Types and Variables directory

--- Python Scripts/Ch2 Data Types and Variables/Ch2 Data Types and Variables/Practice Set/03_Comparison_between_two_numbers.py
+++
@@ -1,6 +0,0 @@
-# Use comparison operators to find out whether a given variable 'a' is greater than 'b' or not.
-# Take a=34 and b=80
-
-a = 34
-b = 80
-print(a>b)
--- Python Scripts/Ch2 Data Types and Variables/Ch2 Data Types and Variables/Practice Set/04_Average_of_two_numbers.py
+++
@@ -1,8 +0,0 @@
-# Write a python program to find average of two numbers enterd by the user.
-
-a = input("Enter First number: ")
-a = int(a) 
-b = input("Enter Second number: ")
-b = int(b)
-avg = (a+b)/2
-print("The average of the numbers is", avg)
